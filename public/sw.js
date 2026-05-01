a3ec774ffd71e49701305a66f687db30764f4bcd
Sunil <skb060968@gmail.com>
2026-05-01 12:48:31 +0530

    version 14

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
    - Update detection + prompt support
    ============================== */
 
-const CACHE_NAME = "tambola-v13";
+const CACHE_NAME = "tambola-v14";
 
 // Pre-cache truly static assets (not Vite-hashed JS/CSS)
 const STATIC_ASSETS = [
